09898d306df833637983c633e5b1dc59a5ffbd99
Diego Herrera <dherrera@binarybag.com>
2018-08-22 12:21:47 -0300

    Avance

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 	// Add scrollspy to <body>
-	$('body').scrollspy({target: ".navbar", offset: 50});
+	$('body').scrollspy({target: ".navbar", offset: 40});
 
 	// Add smooth scrolling on all links inside the navbar
-	$(".navbar a").on('click', function(event) {
-
+	$(".navegacion").on('click', function(event) {
 	  // Make sure this.hash has a value before overriding default behavior
 	  if (this.hash !== "") {
 
@@ -17,18 +16,13 @@ $(document).ready(function(){
 	    // Using jQuery's animate() method to add smooth page scroll
 	    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 	    $('html, body').animate({
-	      scrollTop: $(hash).offset().top
+	      scrollTop: ($(hash).offset().top - 85)
 	    }, 800, function(){
 
-	    // Add hash (#) to URL when done scrolling (default click behavior)
-	      window.location.hash = hash;
 	    });
 
 	  } // End if
 
 	}); 
 
-	$(".navbar").on("activate.bs.scrollspy", function(){
-		console.log(this);
-  })
 });
