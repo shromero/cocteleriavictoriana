09884697fbef0e6c5a0f867914ed782f9e70ab88
Diego Herrera <dherrera@binarybag.com>
2018-08-24 10:42:45 -0300

    Avance

--- js/custom.js
+++ js/custom.js
@@ -33,6 +33,13 @@ $(document).ready(function(){
 			$(this.hash).collapse('show');
 		}
 	});
+	$(".promos-description").on('click', function(event) {
+    event.preventDefault();
+    $(".promos-collapse").each(function(){
+    	$(this).collapse('hide');
+    });
+		$($(this).attr("data-collapse")).collapse('show');
+	});
 
 	
 
@@ -80,6 +87,49 @@ Object.keys(productos_json).forEach(function(key) {
 		producto +=				'</div>';
 		producto +=			'</div>';
 		$('#row_'+key).append(producto);
-})
-})
+	});
+});
+
+var promos_json = JSON.parse(promos);
+Object.keys(promos_json).forEach(function(key) {
+  $.each(promos_json[key], function(index, value){
+
+		numero = index+1
+		var promociones = '<div class="accordion" id="promo_'+key+'_'+numero+'">';
+		promociones +=			'<div class="card">';
+		promociones +=				'<div class="card-header" id="promo_'+key+'_'+numero+'_heading">';
+		promociones +=					'<button class="btn btn-link w-100 promos-description" data-collapse="#promo_'+key+'_'+numero+'_collapse" >';
+		promociones +=						'<p class="float-left mb-0 text-dark">'+value["titulo"]+'</p>';
+		promociones +=						'<p class="float-right mb-0 text-secondary">'+value["precio"]+'</p>';
+		promociones +=					'</a>';
+		promociones +=				'</div>';
+		promociones +=				'<div id="promo_'+key+'_'+numero+'_collapse" class="collapse promos-collapse" aria-labelledby="promo_'+key+'_'+numero+'_heading" data-parent="#promo_'+key+'_'+numero+'">';
+		promociones +=					'<div class="card-body">';
+		promociones +=						'<div class="row">';
+		var cont_promo_1 =					'<div class="col-xs-12 col-md-4">';
+		cont_promo_1 +=								'<img src="images/promociones/'+key+'/'+value["imagen"]+'" class="img-fluid img-responsive">';
+		cont_promo_1 +=							'</div>';
+		var cont_promo_2 =					'<div class="col-xs-12 col-md-8">';
+		cont_promo_2 +=								'<h4>Incluye</h4>';
+		cont_promo_2 +=								'<ul>';
+		$.each(value["productos"], function(index, prod_promo){
+			cont_promo_2 +=								'<li>'+prod_promo["unidades"]+' '+prod_promo["nombre"]+'</li>';
+		});
+		cont_promo_2 +=								'</ul>';
+		cont_promo_2 +=							'</div>';
+		if(numero%2 == 0){
+			promociones +=						cont_promo_2;
+			promociones +=						cont_promo_1;
+		}else{
+			promociones +=						cont_promo_1;
+			promociones +=						cont_promo_2;
+		}
+		promociones +=						'</div>';
+		promociones +=					'</div>';
+		promociones +=				'</div>';
+		promociones +=			'</div>';
+		promociones +=		'</div>';
+		$('#row_promo_'+key).append(promociones);
+	});
+});
 
