d16a74a7baed0d5f6af0dc2078adf975a5ed6601
Diego Herrera <dherrera@binarybag.com>
2018-08-24 15:01:14 -0300

    Avance

--- js/custom.js
+++ js/custom.js
@@ -45,16 +45,6 @@ $(document).ready(function(){
 
 });
 
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById("mapa"), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-
-}
-
 var productos_json = JSON.parse(prods);
 Object.keys(productos_json).forEach(function(key) {
   $.each(productos_json[key], function(index, value){
